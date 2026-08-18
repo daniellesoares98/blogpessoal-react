7b354ea601dbb87b94fef26bd40ad7c34b7b6f66
Danielle Soares <contato.daniellesoares@gmail.com>
2026-08-18 19:42:37 -0300

    💄 UI:  Criação de Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home';
+import Cadastro from './pages/Cadastro';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
     <Navbar />
-    <Home />
+    <Routes>
+      <Route path='/' element= {<Home />} />
+      <Route path='cadastro' element= {<Cadastro />} />
+      <Route path='login' element= {<Login />} />
+    </Routes>
     <Footer />
+    </BrowserRouter>
     </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Navbar(){
     return (
         <>
@@ -5,14 +7,14 @@ function Navbar(){
             <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
 
             <div className="container flex justify-between text-lg mx-8">
-                Blog Pessoal
+                <Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
 
                 <div className="flex gap-4">
                     Postagens
                     Temas
                     Cadastrar tema 
                     Perfil
-                    Sair 
+                    <Link to="/login" className="hover:underline">Sair</Link>
                 </div>
             </div>
         </div>             
