9a871a6cf2af87b4e14b227b50855d9c64de99e7
Danielle Soares <contato.daniellesoares@gmail.com>
2026-08-20 22:13:23 -0300

    feat: estilizações e lógica

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home';
-import Cadastro from './pages/Cadastro';
-import Login from './pages/Login';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
+import FormTema from './components/tema/formtema/FormTema';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element= {<Home />} />
-      <Route path='cadastro' element= {<Cadastro />} />
-      <Route path='login' element= {<Login />} />
-    </Routes>
-    <Footer />
-    </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+            <div className='min-h-[80vh]'>
+              <Routes>
+                <Route path='/home' element={<Home />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastrartema" element={<FormTema />} />
+                <Route path="/editartema/:id" element={<FormTema />} />
+                <Route path="/deletartema/:id" element={<DeletarTema />} />
+              </Routes>
+            </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar(){
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+    handleLogout()
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+}
+
     return (
         <>
 
@@ -11,10 +25,10 @@ function Navbar(){
 
                 <div className="flex gap-4">
                     Postagens
-                    Temas
-                    Cadastrar tema 
+                    <Link to= '/temas' className="hover:underline">Temas</Link>
+                    <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                     Perfil
-                    <Link to="/login" className="hover:underline">Sair</Link>
+                    <Link to='' onClick={logout} className="hover:underline">Sair</Link>
                 </div>
             </div>
         </div>             
